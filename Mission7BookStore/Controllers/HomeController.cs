10a83131fbb86a44fbe40a250296eb12a6e4a68d
nkr22 <81866898+nkr22@users.noreply.github.com>
2022-02-13 18:15:46 -0700

    Add project files.

---
+++ Mission7BookStore/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+//using Mission7BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission7BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        //private BookStoreContext context { get; set; }
+
+        //public HomeController(BookStoreContext temp) => context = temp;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //var blah = context.Books.ToList();
+            return View();
+        }
+
+
+    }
+}
